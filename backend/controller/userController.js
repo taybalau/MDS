347fcba2f17355813e725dd2f5d3c102ee2948b5
taybalau <taynaravitorino@gmail.com>
2023-06-01 18:54:46 -0300

    Adicionando tratamento de erros de usuário

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,34 +1,39 @@
 const userService = require("../service/userService");
 const { getUserByEmail } = require("../service/userService");
+const statusCode = require('../helpers/statusCode');
 
-const addUser = (req, res) => {
-    // Todo : saves user information to supabase
-    // Getting Email and Password from Sign Up Button
-    const user = req.body;
-  
-    userService.createUser(user);
+async function addUser(req, res) {
+  const user = req.body;
 
+  try{
+    var value = await userService.createUser(user);
+
+    if(value == 'Usuário já registrado'){
+      return res.status(statusCode.ALREADY_EXISTS).json({ message: 'USUÁRIO JÁ REGISTRADO' });
+    }
+
+    res.status(statusCode.CREATED).json({ value });
+  } catch (e) {
+
+    res.status(e.status || 500).json({ message: e.message });
+  }
+};
+
+async function getUser(req, res) {
+  const email = req.body;
+
+  var value = await getUserByEmail(email);
+
+  if (value == "Invalid email or password") {
     return res.status(200).json({
-      message: "Users added successfully",
+      message: value,
+    });
+  } else {
+    return res.status(200).json({
+      message: value,
       success: true,
     });
-  };
-
-  async function getUser(req, res) {
-    const email = req.body;
-  
-    var value = await getUserByEmail(email);
-  
-    if (value == "Invalid email or password") {
-      return res.status(200).json({
-        message: value,
-      });
-    } else {
-      return res.status(200).json({
-        message: value,
-        success: true,
-      });
-    }
   }
+}
 
-  module.exports = {addUser, getUser}
+module.exports = { addUser, getUser }
--- backend/service/userService.js
+++ backend/service/userService.js
@@ -1,6 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
-const { User } = require("../models/userModel") 
-var bcrypt = require ('bcrypt')
+const { User } = require("../models/userModel")
+var bcrypt = require('bcrypt')
 
 require("dotenv").config();
 
@@ -9,35 +9,43 @@ const supabase = createClient(
   process.env.DATABASE_KEY
 );
 
-async function createUser(User) {
+const createUser = async (User) => {
 
-    var salt = bcrypt.genSaltSync(10)
-    var encryptedPassword = bcrypt.hashSync(User.password, salt)
-    
+  var salt = bcrypt.genSaltSync(10)
+  var encryptedPassword = bcrypt.hashSync(User.password, salt)
+
   const { data, error } = await supabase
-    .from("Users").insert([{ name: User.name, email: User.email, password: encryptedPassword, 
-        answerOne: User.answerOne }])
+    .from("Users").insert([{
+      name: User.name, email: User.email, password: encryptedPassword,
+      answerOne: User.answerOne, answerTwo: User.answerTwo
+    }])
+
 
   if (error) {
-    console.log(error);
-    return data;
-  }
-}
-
-async function getUserByEmail(user) {
-  const { data, error } = await supabase
-    .from("Users")
-    .select("name")
-    .eq('email', user.email)
-
-  if (error) {
-    console.log(error);
-    return error;
-  } else if (data.length === 0) {
-    return "Invalid email or password";
+    if (error.code == '23505') {
+      return 'Usuário já registrado';
+    } else {
+      throw new error;
+    }
   } else {
     return data;
   }
 }
 
-module.exports = { createUser, getUserByEmail };
+  async function getUserByEmail(user) {
+    const { data, error } = await supabase
+      .from("Users")
+      .select("name")
+      .eq('email', user.email)
+
+    if (error) {
+      console.log(error);
+      return error;
+    } else if (data.length === 0) {
+      return "Invalid email or password";
+    } else {
+      return data;
+    }
+  }
+
+  module.exports = { createUser, getUserByEmail };
